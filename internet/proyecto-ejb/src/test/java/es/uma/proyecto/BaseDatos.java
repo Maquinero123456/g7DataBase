1779560be2336c116de3536cc10f17553338d78e
Maquinero123456 <jimenanavarrodavid@uma.es>
2022-04-23 19:32:52 +0200

    Carpeta java

---
+++ internet/proyecto-ejb/src/test/java/es/uma/proyecto/BaseDatos.java
@@ -0,0 +1,30 @@
+package es.uma.informatica.sii.ejb.practica;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import es.uma.informatica.sii.ejb.practica.entidades.Ingrediente;
+import es.uma.informatica.sii.ejb.practica.entidades.Lote;
+import es.uma.informatica.sii.ejb.practica.entidades.Producto;
+
+public class BaseDatos {
+	public static void inicializaBaseDatos(String nombreUnidadPersistencia) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(nombreUnidadPersistencia);
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		//No tocar arriba
+		
+
+		//No tocar abajo
+		em.close();
+		emf.close();
+	}
+}
---
+++ internet/proyecto-ejb/src/test/java/es/uma/proyecto/SuiteTest.java
@@ -0,0 +1,41 @@
+package es.uma.informatica.sii.ejb.practica;
+
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class SuiteTest {
+	
+	private static final String GLASSFISH_CONFIGI_FILE_PROPERTY = "org.glassfish.ejb.embedded.glassfish.configuration.file";
+	private static final String CONFIG_FILE = "target/test-classes/META-INF/domain.xml";
+	
+	public static EJBContainer ejbContainer;
+	public static Context ctx;
+	
+	
+	@BeforeClass
+	public static void setUpClass() {
+		Properties properties = new Properties();
+		properties.setProperty(GLASSFISH_CONFIGI_FILE_PROPERTY, CONFIG_FILE);
+		ejbContainer = EJBContainer.createEJBContainer(properties);
+		ctx = ejbContainer.getContext();
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		if (ejbContainer != null) {
+			ejbContainer.close();
+		}
+	}
+
+}
