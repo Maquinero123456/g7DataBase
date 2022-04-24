70dee895a11b5be4bb672bb968250ac4cd506670
proposisional <a@uma.es>
2022-04-23 20:53:35 -0400

    /u

--- internet/proyecto-ejb/src/main/java/es/uma/proyecto/exceptions/TrazabilidadException.java
+++ internet/proyecto-ejb/src/main/java/es/uma/proyecto/exceptions/TrazabilidadException.java
@@ -2,7 +2,9 @@ package es.uma.proyecto.exceptions;
 
 public class TrazabilidadException extends Exception {
 	
-	public TrazabilidadException () {};
+	public TrazabilidadException () {
+		
+	};
 	
 	public TrazabilidadException(String message) {
 		super(message);
