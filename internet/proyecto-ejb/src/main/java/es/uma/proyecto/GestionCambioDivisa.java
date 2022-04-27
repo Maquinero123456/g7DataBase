d533d6be0b15488d5e9ace9f63f0a87e71179e72
Maquinero123456 <jimenanavarrodavid@uma.es>
2022-04-27 21:09:00 +0200

    CambioDivisa JavaDoc

--- internet/proyecto-ejb/src/main/java/es/uma/proyecto/GestionCambioDivisa.java
+++ internet/proyecto-ejb/src/main/java/es/uma/proyecto/GestionCambioDivisa.java
@@ -15,6 +15,10 @@ public interface GestionCambioDivisa {
      * El cambio de divisas se considerará una transacción especial donde el origen y destino es la misma cuenta. 
      * Para poder realizar un cambio de divisas será necesario que la cuenta tenga saldos en las divisas de origen y destino.
      * Los saldos de las cuentas asociadas con la cuenta agrupada deberá actualizarse también. No será posible realizar un cambio de divisas en cuentas segregadas.
+     * @param pooled Cuenta de la que queremos cambiar saldo
+     * @param original Divisa de la cuenta que quiero cambiar saldo
+     * @param nueva Divisa a la que quiero cambiar
+     * @param cantidadDeseada Cantidad de la nueva divisa que quiero
      * @throws CuentaReferenciaException
      */
     public void cambioDivisas(PooledAccount pooled, Divisa original, Divisa nueva, Double cantidadDeseada) throws PooledAccountException, DivisaException, SaldoException, CuentaReferenciaException, CuentaReferenciaException;
