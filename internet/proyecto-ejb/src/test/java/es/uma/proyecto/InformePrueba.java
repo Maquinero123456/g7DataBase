3c5c76ac96bf33b647d2d6488c7aab4e62bbceb6
Maquinero123456 <jimenanavarrodavid@uma.es>
2022-04-30 15:35:44 +0200

    InformePrueba

--- internet/proyecto-ejb/src/test/java/es/uma/proyecto/InformePrueba.java
+++ internet/proyecto-ejb/src/test/java/es/uma/proyecto/InformePrueba.java
@@ -1,5 +1,6 @@
 package es.uma.proyecto;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import org.junit.Test;
 import es.uma.proyecto.entidades.Cliente;
 import es.uma.proyecto.entidades.Cuenta;
 import es.uma.proyecto.exceptions.ClienteException;
+import es.uma.proyecto.exceptions.CuentaException;
 
 public class InformePrueba {
     
@@ -20,24 +22,39 @@ public class InformePrueba {
 
 	private static final String INFORMES_EJB = "java:global/classes/Informes";
 	private static final String UNIDAD_PERSITENCIA_PRUEBAS = "proyectoTest";
-	
+    private static final String CLIENTES_EJB = "java:global/classes/Clientes";
+    private static final String CUENTAS_EJB = "java:global/classes/Cuentas";
+
 	private GestionInformes gestionInformes;
+    private GestionClientes gestionClientes;
+    private GestionCuentas gestionCuentas;
 
     @Before
 	public void setup() throws NamingException  {
+        gestionCuentas = (GestionCuentas) SuiteTest.ctx.lookup(CUENTAS_EJB);
 		gestionInformes = (GestionInformes) SuiteTest.ctx.lookup(INFORMES_EJB);
-		BaseDatos.inicializaBaseDatos(UNIDAD_PERSITENCIA_PRUEBAS);
+		gestionClientes = (GestionClientes) SuiteTest.ctx.lookup(CLIENTES_EJB);
+        BaseDatos.inicializaBaseDatos(UNIDAD_PERSITENCIA_PRUEBAS);
 	}
 
 
     @Test
-    public void informeHolanda() throws ClienteException{
-        /*List<Cliente> cliente = gestionInformes.informeCuentasPaisesBajos();
-        for(Cliente e : cliente){
-            if(!e.getPais().equals("PaisesBajos")){
-                fail("Solo deberia devolver clientes de holanda");
-            }
-        }*/
+    public void informeHolanda() throws ClienteException, CuentaException{
+        Cliente client = gestionClientes.getCliente("uno");
+        if(client == null){
+            throw new ClienteException("El cliente deberia existir");
+        }
+        Cuenta cuenta = gestionCuentas.getCuenta("1");
+        if(cuenta == null){
+            throw new CuentaException("La cuenta deberia existir");
+        }
+
+        List<String> report = gestionInformes.informeCuentasPaisesBajos(true, "1");
+
+        List<String> seSuponeQueDaEsto = null;
+
+        assertEquals(report, seSuponeQueDaEsto);
+
     }
 
     @Test
