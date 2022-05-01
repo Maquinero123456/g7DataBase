acf5c6a6ed4f4d892f95d5c42961325e93d629ed
proposisional <a@uma.es>
2022-05-01 12:43:15 -0400

    Anotaciones transaccion Requisito 14

--- internet/proyecto-ejb/src/test/java/es/uma/proyecto/TransaccionPrueba.java
+++ internet/proyecto-ejb/src/test/java/es/uma/proyecto/TransaccionPrueba.java
@@ -12,6 +12,7 @@ import javax.naming.NamingException;
 import org.junit.Before;
 import org.junit.Test;
 
+import es.uma.informatica.sii.anotaciones.Requisitos;
 import es.uma.proyecto.entidades.CuentaReferencia;
 import es.uma.proyecto.exceptions.ClienteException;
 import es.uma.proyecto.exceptions.CuentaException;
@@ -47,7 +48,15 @@ public class TransaccionPrueba {
         gestionCuentas = (GestionCuentas) SuiteTest.ctx.lookup(CUENTAS_EJB);
     }
 
-    @Test
+    @Requisitos({"RF14"})
+    @Test 
+    /**
+     * Prueba para la correcta transaccion de un Individual 
+     * Comprueba si las cuentas de referencia que participan en la transaccion existen
+     * Finalmente, las cuentas deben tener el dinero correspondiente
+     * Sumandole a la destino y quitandole a la origen 
+     * @throws ClienteException, CuentaException, IndividualException, SaldoException
+     */
     public void testTransaccionIndividual() throws ClienteException, CuentaException, IndividualException, SaldoException{
         generarTransaccion.transaccionIndividual("Paco", "cuentaOrigen", "cuentaDestino", 100.0, "Quien sabe");
         CuentaReferencia ref = null;
@@ -57,10 +66,26 @@ public class TransaccionPrueba {
             fail("La cuenta deberia existir");
         }
 
+        CuentaReferencia ref2 = null;
+        try{
+            ref2 = gestionCuentas.getCuentaReferencia("autorizadoDestino");
+        }catch(CuentaException e){
+            fail("La cuenta deberia existir");
+        }
+
         assertTrue(200.0 == ref.getSaldo());
+        assertTrue(900.0 == ref2.getSaldo());
     }
 
-    @Test
+    @Requisitos({"RF14"})
+    @Test 
+    /**
+     * Prueba para la correcta transaccion de un autorizado 
+     * Comprueba si las cuentas de referencia que participan en la transaccion existen
+     * Finalmente, las cuentas deben tener el dinero correspondiente
+     * Sumandole a la destino y quitandole a la origen 
+     * @throws ClienteException, CuentaException, IndividualException, SaldoException
+     */
     public void testTransaccionAutorizada() throws ClienteException, CuentaException, IndividualException, SaldoException{
         generarTransaccion.transaccionAutorizado("Pablo", "autorizadoOrigen", "autorizadoDestino", 100.0, "Quien sabe");
         CuentaReferencia ref = null;
@@ -69,7 +94,15 @@ public class TransaccionPrueba {
         }catch(CuentaException e){
             fail("La cuenta deberia existir");
         }
+        
+        CuentaReferencia ref2 = null;
+        try{
+            ref2 = gestionCuentas.getCuentaReferencia("autorizadoDestino");
+        }catch(CuentaException e){
+            fail("La cuenta deberia existir");
+        }
 
         assertTrue(200.0 == ref.getSaldo());
+        assertTrue(900.0 == ref2.getSaldo());
     }
 }
