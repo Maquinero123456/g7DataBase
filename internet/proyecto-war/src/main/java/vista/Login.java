2cbd18a797fe16db75b4dbab921b7cd709b6589d
Maquinero123456 <jimenanavarrodavid@uma.es>
2022-05-21 13:26:34 +0200

    Anotaciones-sii borrado

--- internet/proyecto-war/src/main/java/vista/Login.java
+++ internet/proyecto-war/src/main/java/vista/Login.java
@@ -26,6 +26,7 @@ public class Login {
     @Inject
     @EJB
     private GestionCuentasUsuarios cuentas;
+    @EJB 
     private GestionAdministratitivos administratitivos;
 
     private Usuario usuario;
